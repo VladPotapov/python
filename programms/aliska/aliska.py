ba6f45c2efe04b0e2d65f49b0d9bbbfd81deb7af
KotelnikovRoman <kotelnikovrm@gmail.com>
2019-11-16 20:29:57 +0800

    добавление классов и проверка на вопросы

--- programms/aliska/aliska.py
+++ programms/aliska/aliska.py
@@ -1,8 +1,37 @@
 class Question(object):
-    """вопрос пользователя"""
+    """
+    запрашивает вопрос
+    и сохраняет результат
+    передавая Алиске
+    """
+    def question(self):
+        """вопрос пользователя"""
+        ask = input("вопрос ").lower()
+        return ask
 
 class Answer(object):
     """ответ на вопрос"""
+    def answer(self, user):
+        """
+        запрашивает вопрос пользователя
+        и возвращает ответ
+        если вопроса нет запрашивает снова
+        выходит из цикла после слова пока
+        """
+        while True:
+            request = user.question()
+            if request == "привет":
+                print("И тебе привет")
+            elif request == "":
+                print("дружище ты забыл задать вопрос")
+            elif request == "пока":
+                print("до встречи")
+                break
+            else:
+                print("чё чё чё?")
+    
 
 user = Question()
 aliska = Answer()
+
+aliska.answer(user)
