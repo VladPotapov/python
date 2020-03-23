934b2304f2308fa581c9c31630a8ebbc81c0e75f
Roman Kotelnikov <rproger@mail.ru>
2020-03-23 20:53:46 +0800

    upgrade lecture and python_source
    Обновил скрипт в каталоге 9 папки python_source.
    Задача: если числа в Player and Alien совпадут убить пришельца

--- python_source/chapter09/my_code/script.py
+++ python_source/chapter09/my_code/script.py
@@ -1,6 +1,30 @@
-import games
+# -*- coding: utf-8 -*-
+import random
+import time
+class Player(object):
+    #игрок
+    def blast(self, enemi):
+        print('Игрок стреляет во врага')
+        time.sleep(1)
+        print(3)
+        time.sleep(1)
+        print(2)
+        time.sleep(1)
+        print(1)
+        enemi.die()
 
-user = games.Player("Djo", 5000)
-summ = games.ask_number(num, 100, 1000)
+class Alien(object):
+    #пришелец
+    def die(self):
+        print('Пришелец: О нет меня подстрелили\n Как же так, я умираю')
+        print('Я больше никогда не увижу своих миллионных потомков')
+        print('Но они прилетят чтобы отомстить за меня вам людишки')
 
-print(summ)
+print('\t\t\t"ГИБЕЛЬ ПРИШЕЛЬЦА"')
+
+hero = Player()
+invaid = Alien()
+
+hero.blast(invaid)
+
+input("\nEnter")
