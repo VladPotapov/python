3f49b83486a14bb939e70b1117972585447b9dae
vladPotapov <rproger@mail.ru>
2020-03-27 06:07:48 +0800

    add function yes_no and playGame in script chapte 9

--- python_source/chapter09/my_code/script.py
+++ python_source/chapter09/my_code/script.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# version 2.0
 
 import time
 import random
@@ -6,12 +7,40 @@ import random
 def randNum():
     return random.randint(0, 1)
 
+def yes_no(que):
+    res = None
+
+    while res not in ("y", "n", "д", "н"):
+        res = input(que).lower()
+    
+    return res
+
+def playGame(gamer):
+    choice = None
+
+    while choice != '0':
+        print(
+            """
+            0 выход
+            1 предупредительный выстрел
+            2 стрелять на поражение
+            """
+        )
+
+        choice = input('ваш выбор: ')
+
+        if choice == '0':
+            print('Пока')
+            break
+        elif choice == '1':
+            gamer.caution(invader)
+        elif choice == '2':
+            gamer.blast(invader)
+            break
+
 
 class Player(object):
     """игрок"""
-
-    num = randNum()
-
     def caution(self, enemy):
         print("предупредительный выстрел")
         print('Немедленно покиньте зону земли')
@@ -24,11 +53,11 @@ class Player(object):
 class Alien(object):
     """Враждебный инопланетянен"""
 
-    num = randNum()
-
     def die(self, play, ali):
 
-        if play.num == ali.num:
+        # создать случайные числа и сравнить 
+        # для выбора ответа
+        if randNum() == randNum():
             print("Ну вот и всё спета песенка моя")
             time.sleep(1)
             print("Передай моим личинкам живущим за 1000 световых лет")
@@ -36,12 +65,14 @@ class Alien(object):
             print("Что я их любил")
             time.sleep(1)
             print("А теперь прощай безжалостный мир")
+            print("Ура пришелец убит и мир может спать спокойно")
         else:
             print("Ага промохнулся")
             time.sleep(1)
             print("Вот тебе получай")
             time.sleep(1)
             print("тюх-тюх, пах-пах, БАБАААХ!!!")
+            print("Увы, вы промахнулись и теперь весь мир обречён на гибель")
 
     def mockery(self):
         print('Ха-ха-ха, они думают что я испугаюсь их пуколок')
@@ -50,26 +81,9 @@ hero = Player()
 invader = Alien()
 
 def main():
-    choice = None
-
-    while choice != '0':
-        print(
-            """
-            0 выход
-            1 предупридительный выстрел
-            2 стрелять на поражение
-            """
-        )
-
-        choice = input('Выбор ')
-
-        if choice == '0':
-            print('Пока')
-            break
-        elif choice == '1':
-            hero.caution(invader)
-        elif choice == '2':
-            hero.blast(invader)
-            break
+    play = None
+    while play != "n" or "н":
+        playGame(hero)
+        play = yes_no("Хотите сыграть ещё раз? д / н ")
 
 main()
