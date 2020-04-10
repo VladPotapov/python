862b2093ef0686f83182b641bebbef55543acfe6
Roman Kotelnikov <rproger@mail.ru>
2020-04-11 04:48:22 +0800

    update file what is your name in  python_c++

--- python_C++/path1/my_code/2/2-what-is-your-name.py
+++ python_C++/path1/my_code/2/2-what-is-your-name.py
@@ -9,4 +9,14 @@ name = "Сократ"
 name = input("Как тебя зовут? ")
 print("Привет", name, "!")
 
+for i in range(len(name)):
+    print(i, "=", name[i])
+
+print("Наоборот")
+
+N = len(name)
+for i in range(N):
+    print(name[N - 1])
+    N -= 1
+
 input("\nEnter")
---
+++ python_C++/path1/my_code/2/script.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8
+
+name = "Roma"
+age = 35
+rost = 1.45
+boo = True
+
+print(type(name))
+print(type(age))
+print(type(rost))
+print(type(boo))
