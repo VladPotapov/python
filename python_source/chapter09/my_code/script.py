cbdbf5312eec475f13f91af39c41066ac68f50a6
Roman <kotelnikovrm@gmail.com>
2020-07-12 19:39:23 +0800

    python source - cards.py

--- python_source/chapter09/my_code/script.py
+++ python_source/chapter09/my_code/script.py
@@ -65,3 +65,17 @@ class Deck(Hand):
     def shuffle(self):
         import random
         random.shuffle(self.cards)
+
+    def deal(self, hands, per_hand=1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card, hand)
+                else:
+                    print("Не могу больше сдавать карты")
+
+
+if __name__ == "__main__":
+    print("Это модуль содержащий классы для карточных игр")
+    print("\nEnter")
