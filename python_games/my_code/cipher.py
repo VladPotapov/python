dceb8579b06c1f75225fe561568853b3f03c03f5
Roman <kotelnikovrm@gmail.com>
2020-07-29 22:04:04 +0800

    plus index

--- python_games/my_code/cipher.py
+++ python_games/my_code/cipher.py
@@ -1,6 +1,8 @@
 SYMBOLS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 MAX_KEY_SIZE = len(SYMBOLS)
 
+# функция для определения шифровки или расшифровки
+
 
 def getMode():
     while True:
@@ -9,3 +11,26 @@ def getMode():
 
         if mode in ['зашифровать', 'з', 'расшифровать', 'р']:
             return mode
+        else:
+            print('Введите "зашифровать" или "з" чтоб зашифровать')
+            print('Введите "расшифровать" или "р" чтоб расшифровать')
+
+# принимает текст для обработки
+
+
+def getMessage():
+    print("Введите текст: ")
+    return input()
+
+# принимает ключ
+
+
+def getKey():
+    key = 0
+
+    while True:
+        print("Введите ключ шифрования (1-%s)" % (MAX_KEY_SIZE))
+        key = int(input())
+
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
